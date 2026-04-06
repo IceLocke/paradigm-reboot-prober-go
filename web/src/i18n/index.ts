2973e2b2b9c273c77a708f50823d99797025dc98
IceLocke <lockeice@gmail.com>
2026-04-06 23:20:38 +0800

    feat: jp i18n (#50)

--- web/src/i18n/index.ts
+++ web/src/i18n/index.ts
@@ -1,14 +1,21 @@
 import { createI18n } from 'vue-i18n'
 import en from './en'
+import ja from './ja'
 import zh from './zh'
 
 const navLang = navigator.language.substring(0, 2)
 
+function detectLocale(): string {
+  if (navLang === 'zh') return 'zh'
+  if (navLang === 'ja') return 'ja'
+  return 'en'
+}
+
 const i18n = createI18n({
-  locale: navLang === 'zh' ? 'zh' : 'en',
+  locale: detectLocale(),
   fallbackLocale: 'en',
   legacy: false,
-  messages: { en, zh },
+  messages: { en, ja, zh },
 })
 
 export default i18n
